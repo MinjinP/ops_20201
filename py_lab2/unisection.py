f0bfbb4c207aa028ba67743755433e2b58f6405d
MinjinP <51973581+MinjinP@users.noreply.github.com>
2020-04-29 21:19:20 +0900

    Create unisection.py

---
+++ py_lab2/unisection.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import re
+
+def unisection():
+    a = input('lst list: ')
+    b = input('2nd list: ')
+    
+    A = re.sub('[-=+,#/\?:^$.@*’\'\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', ' ', a)
+    B = re.sub('[-=+,#/\?:^$.@*’\'\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', ' ', b)
+    
+    A = A.split()
+    B = B.split()
+
+    set1 = set(A)
+    set2 = set(B)
+
+    print("=> union ", set1&set2)
+    print("=> intersection ", set1|set2)
